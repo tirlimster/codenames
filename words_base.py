aba1fccfb28ba3b4787476236fab758318ecef25
Yulik Daniel <yulik.daniel@gmail.com>
2020-04-22 17:26:19 +0300

    Added words

---
+++ words_base.py
@@ -0,0 +1,33 @@
+from json import loads, dumps
+from random import shuffle, seed
+
+def removeRepetitions():
+	with open("words.json", 'r', encoding="utf-8") as f:
+		ws = loads(f.read().strip())
+		ws = [x.lower() for x in ws]
+	ws = list(set(ws))
+	with open("words.json", 'w', encoding="utf-8") as f:
+		f.write(dumps(ws))
+
+
+def addList(list):
+	list = list.strip().split('\n')
+	with open("words.json", 'r', encoding="utf-8") as f:
+		ws = loads(f.read().strip())
+	with open("words.json", 'w', encoding="utf-8") as f:
+		f.write(dumps(ws + list))
+	removeRepetitions()
+
+
+def getList():
+	with open("words.json", 'r', encoding="utf-8") as f:
+		ws = loads(f.read().strip())
+	return ws
+
+
+def getWords(n, key=1543):
+	seed(key)
+	with open("words.json", 'r', encoding="utf-8") as f:
+		ws = loads(f.read().strip())
+	shuffle(ws)
+	return ws[:n]
