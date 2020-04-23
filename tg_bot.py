038191b04c6f37d7bd128e026711753ad93720f4
Yulik Daniel <yulik.daniel@gmail.com>
2020-04-23 16:22:23 +0300

    Working on tg_bot

--- tg_bot.py
+++ tg_bot.py
@@ -1,5 +1,7 @@
 from config import Config
+from threading import Thread
 from telebot import TeleBot
+from time import sleep
 config_secrets = Config()
 
 
@@ -8,13 +10,30 @@ class TgBot:
     events = []
 
     def __init__(self, events):
-        self.events = events
+        self.events.append(events)
+
+    def mainloop(self):
         self.bot.polling()
 
+    def show(self):
+        print(self.events[0])
+
     @bot.message_handler(func=lambda x: True)
-    def start(mes, events=events):
-        events.append({"text": mes.text})
+    def parse_message(mes, events=events):
+        events[0].append({
+            "text": mes.text,
+            "first": mes.from_user.first_name,
+            "last": mes.from_user.last_name,
+            "id": mes.from_user.id,
+            "platform": "tg"
+            })
+
 
 if __name__ == "__main__":
     ar = []
-    B = TgBot(ar)
+    BOT = TgBot(ar)
+    t1 = Thread(target=BOT.mainloop)
+    t1.start()
+    while True:
+        print(ar)
+        sleep(1)
